356e4407b179b19a90339b56585cd948a53c0b9c
FJss23 <uo252473@uniovi.es>
2020-10-04 23:07:42 +0200

    restart repo

--- src/components/Section.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import sectionStyle from './section.module.css';
-
-export default function Section({ children }) {
-  return <div className={sectionStyle.section}>{children}</div>;
-}
